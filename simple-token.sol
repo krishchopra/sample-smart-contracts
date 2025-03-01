7b658fad47acd67c9cef7d73c232ed04854f0176
Krish Chopra <krish.chopra23@gmail.com>
2025-03-01 05:46:45 -0500

    Create simple-token.sol

---
+++ simple-token.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleToken {
+    mapping(address => uint256) public balances;
+    uint256 public totalSupply;
+    address public owner;
+    
+    constructor(uint256 _initialSupply) {
+        totalSupply = _initialSupply;
+        balances[msg.sender] = _initialSupply;
+        owner = msg.sender;
+    }
+    
+    function transfer(address _to, uint256 _amount) external {
+        require(balances[msg.sender] >= _amount, "Insufficient balance");
+        balances[msg.sender] -= _amount;
+        balances[_to] += _amount;
+    }
+    
+    function withdraw(uint256 _amount) external {
+        require(balances[msg.sender] >= _amount, "Insufficient balance");
+        (bool success, ) = msg.sender.call{value: _amount}("");
+        require(success, "Transfer failed");
+        balances[msg.sender] -= _amount;
+    }
+}
